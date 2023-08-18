a9d2c4dcf2c27414274801fac2f35aaba3ea43b5
daudiqbal951 <daudiqbal951@yahoo.com>
2023-08-18 17:45:20 +0500

    assignment 01

---
+++ assignment01.js
@@ -0,0 +1,32 @@
+"use strict";
+// Printing the message
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT");
+// Storing name in a variable and Printing it
+var username = ("Daud Iqbal");
+console.log(username);
+// Storing 10 numbers in difference variables
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+var num5 = 5;
+var num6 = 6;
+var num7 = 7;
+var num8 = 8;
+var num9 = 9;
+var num10 = 10;
+// Printing the sum of all numbers stored
+var sum = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
+console.log(sum);
+// Printing the difference of all numbers stored
+var difference = (num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10);
+console.log(difference);
+// Printing the product of all numbers stored
+var product = (num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10);
+console.log(product);
+// Printing the quotient of any two numbers stored
+var quotient = (num10 / num5);
+console.log(quotient);
+// Performing all operations at once and observe in which the operations are exceuted
+var check = (num10 + num3 - num5 * num7 / num2);
+console.log(check);
---
+++ assignment01.ts
@@ -0,0 +1,32 @@
+// Printing the message
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT")
+// Storing name in a variable and Printing it
+var username:string=("Daud Iqbal")
+console.log(username)
+// Storing 10 numbers in difference variables
+var num1:number=1
+var num2:number=2
+var num3:number=3
+var num4:number=4
+var num5:number=5
+var num6:number=6
+var num7:number=7
+var num8:number=8
+var num9:number=9
+var num10:number=10
+// Printing the sum of all numbers stored
+var sum:number=(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)
+console.log(sum)
+// Printing the difference of all numbers stored
+var difference:number=(num1-num2-num3-num4-num5-num6-num7-num8-num9-num10)
+console.log(difference)
+// Printing the product of all numbers stored
+var product:number=(num1*num2*num3*num4*num5*num6*num7*num8*num9*num10)
+console.log(product)
+// Printing the quotient of any two numbers stored
+var quotient:number=(num10/num5)
+console.log(quotient)
+// Performing all operations at once and observe in which the operations are exceuted
+var check:number=(num10+num3-num5*num7/num2)
+console.log(check)
+// The operations are executed according to the DMAS Rule
